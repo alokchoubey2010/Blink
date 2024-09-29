241c7b54ca23961a4aba29a7118cb5378de2a946
alokchoubey892 <alokchoubey892@gamil.com>
2024-09-29 16:38:09 +0530

    update

---
+++ auth.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC6ULXUTsCznJgZmOQDTv43Fb9fOUwLNE8",
+  authDomain: "blink-c9c7e.firebaseapp.com",
+  databaseURL: "https://blink-c9c7e-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "blink-c9c7e",
+  storageBucket: "blink-c9c7e.appspot.com",
+  messagingSenderId: "775297212661",
+  appId: "1:775297212661:web:f7c05b401d979c163025bc",
+  measurementId: "G-TLRMJHVHNZ"
+};
