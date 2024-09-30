4125907fd3fd9b31d08f69da4f6a8abe5867c33b
alokchoubey892 <alokchoubey892@gamil.com>
2024-09-30 18:31:07 +0530

    Added google login

--- auth.js
+++ auth.js
@@ -8,3 +8,38 @@ const firebaseConfig = {
   appId: "1:775297212661:web:f7c05b401d979c163025bc",
   measurementId: "G-TLRMJHVHNZ"
 };
+
+firebase.initializeApp(firebaseConfig);
+
+google = document.getElementById("google");
+google.onclick = function(){
+  google.innerHTML = '';
+  google.innerHTML += '<div class="loader"></div>';
+  auth = firebase.auth();
+   const provider = new firebase.auth.GoogleAuthProvider();
+
+ firebase.auth().signInWithPopup(provider)
+   .then((user) => {
+      updateToDatabase(user);
+   })
+   .catch((error) => {
+     alert("Google sign-in failed: " + error.message);
+   });
+}
+
+function updateToDatabase(user) {
+  updates = {
+    name: user.displayName,
+    email: user.email,
+    profileImg: user.photoURL,
+    lastLogin: new Date(),
+    lastLogout: "null",
+    blinks: '0',
+    bio: 'null',
+    followers: '0',
+    following: '0',
+    streak: '0'
+  }
+  database = firebase.database();
+  database.ref('users/' + user.displayName).update(updates)
+}
